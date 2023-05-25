caf943b770073efc45b598cea1a55cccca1d61bb
Theodore Lesser <ted.a.lesser@gmail.com>
2023-05-25 10:06:26 -0400

    Adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+function destructivelyAppendCat (name) {
+    cats.push ("Ralph")
+}
+
+function destructivelyPrependCat (name) {
+    cats.unshift ("Bob")
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop ("Ralph")
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift ("Bob")
+}
+function appendCat(name) {
+    const newCatArray = [...cats, "Broom"];
+    return newCatArray;
+}
+console.log(appendCat());
+
+function prependCat(name) {
+    const newCatArray = ["Arnold", ...cats];
+    return newCatArray;
+}
+console.log(prependCat());
+
+function removeFirstCat(name) {
+    const newCatArray = cats.slice(1);
+    return newCatArray
+}
+console.log(removeFirstCat())
+
+function removeLastCat(){
+    const newCatArray = cats.slice(0, -1);
+    return newCatArray;
+}
+console.log(removeLastCat())
+
+beforeEach(function () {
+    cats.length = 0;
+    cats.push("Milo","Otis","Garfield");
+});
+
+
